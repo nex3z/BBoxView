1e9d35585e3874e5987c06547b5b834fc6b37067
nex3z <litianxing9@gmail.com>
2020-09-26 14:44:15 +0800

    Update sample.

--- sample/src/main/java/com/nex3z/bboxview/sample/MainActivity.kt
+++ sample/src/main/java/com/nex3z/bboxview/sample/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.nex3z.bboxview.sample
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -12,8 +14,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun init() {
-        btn_clear.setOnClickListener {
-            bbav_annotation.clear()
+        btn_overlay.setOnClickListener {
+            Intent(this, BBoxOverlayActivity::class.java).apply(this::startActivity)
         }
     }
 }
